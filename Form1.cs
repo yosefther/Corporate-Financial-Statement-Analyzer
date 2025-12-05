d98755eaea40a3a58bcfbf2469d1d492d4e2b309
yosefther <yousefthair9@gmail.com>
2025-12-05 22:28:32 +0300

    Refactor Form1 UI and enhance button interactions
    - Added `System.Runtime.CompilerServices` namespace.
    - Introduced `RGBColors` struct for static color definitions.
    - Refactored `Form1` constructor to initialize `leftBorderBtn`.
    - Added `ActivteButton`, `DisableButton`, and `DisableAndEnableButtons`
      methods for dynamic button activation/deactivation.
    - Reintroduced and updated event handlers for button interactions.
    - Added new event handlers for additional UI elements.
    - Improved UI feedback and organization of event handling logic.

--- Form1.cs
+++ Form1.cs
@@ -17,18 +17,6 @@ namespace Corporate_Financial_Statement_Analyzer
 {
     public partial class Form1 : Form
     {
-
-        private IconButton currentBtn;
-        private Panel leftBorderBtn;
-
-        public Form1()
-        {
-            InitializeComponent();
-            leftBorderBtn = new Panel();
-            leftBorderBtn.Size = new Size(7, 60);
-            //leftBorderBtn.contr
-        }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {}
         private void BtnLoad_Click(object sender, EventArgs e) {}
         private void _incomeStatmentSave_Click(object sender, EventArgs e) {}
@@ -41,11 +29,33 @@ namespace Corporate_Financial_Statement_Analyzer
         private void label1_Click(object sender, EventArgs e) {}
         private void label3_Click(object sender, EventArgs e){}
         private void button1_Click(object sender, EventArgs e){}
-        private void iconButton1_Click(object sender, EventArgs e){}
-        private void iconButton4_Click(object sender, EventArgs e){}
-        private void iconButton3_Click(object sender, EventArgs e){}
-        private void AboutButton_Click(object sender, EventArgs e){}
         private void panel1_Paint(object sender, PaintEventArgs e){}
+        private void iconPictureBox1_Click(object sender, EventArgs e){}
+        private void pictureBox1_Click_1(object sender, EventArgs e){}
+        private void label4_Click(object sender, EventArgs e){}
+
+
+
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
+
+        }
 
 
         private void button2_Click(object sender, EventArgs e){
@@ -64,5 +74,74 @@ namespace Corporate_Financial_Statement_Analyzer
             CSVLoader CashFlowCSV = new CSVLoader();
             dataGridView1.DataSource = CashFlowCSV.buttonClick_LoadCSV();
         }
+
+        private void ActivteButton(object senderButn , Color color)
+        {
+            if (senderButn != null)
+            {
+                currentBtn = (IconButton)senderButn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+                leftBorderBtn.Visible = false;
+            }
+        }
+
+        private void DisableAndEnableButtons(object sender,Color color)
+        {
+            if (!(sender is IconButton clickedBtn)) return;
+
+            if (currentBtn != clickedBtn)
+            {
+                DisableButton();                      
+                ActivteButton(clickedBtn,color);
+            }
+            else
+            {
+                DisableButton();
+                currentBtn = null;
+            }
+        }
+
+        private void MainButton_Click(object sender, EventArgs e)
+        {
+            DisableAndEnableButtons(sender , RGBColors.color1);
+        }
+        private void iconButton3_Click(object sender, EventArgs e)
+        {
+            DisableAndEnableButtons(sender, RGBColors.color2);
+        }
+        private void iconButton4_Click(object sender, EventArgs e)
+        {
+            DisableAndEnableButtons(sender, RGBColors.color3);
+        }
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            DisableAndEnableButtons(sender, RGBColors.color4);
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            DisableAndEnableButtons(sender, RGBColors.color5);
+        }
+
     }
 }
